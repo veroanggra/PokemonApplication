837f14db1b27902434eba082bac786a4c3f25a92
veroanggra <veronicaputri497@gmail.com>
2021-10-30 19:53:50 +0700

    [ADD] Splashscreen

--- app/src/main/java/com/veronica/idn/pokemonapplication/MainActivity.kt
+++ app/src/main/java/com/veronica/idn/pokemonapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.veronica.idn.pokemonapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    companion object{
+        fun getLaunchService(from: Context) =
+            Intent(from, MainActivity::class.java).apply {
+                addFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+                            or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                )
+            }
     }
 }
---
+++ app/src/main/java/com/veronica/idn/pokemonapplication/presentation/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.veronica.idn.pokemonapplication.presentation.splash
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.veronica.idn.pokemonapplication.MainActivity
+import com.veronica.idn.pokemonapplication.R
+import com.veronica.idn.pokemonapplication.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var splashBinding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+        goToMain()
+    }
+
+    private fun goToMain() {
+        Handler(mainLooper).postDelayed({
+            startActivity(MainActivity.getLaunchService(this))
+            finish()
+        }, 3000)
+    }
+}
